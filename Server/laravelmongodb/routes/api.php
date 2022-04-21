e8565996feaeb12f85553e726a56606102aed996
ngoquangdatjpnd <66765392+ngoquangdatjpnd@users.noreply.github.com>
2022-04-21 11:46:12 +0700

    xóa file thừa

--- Server/laravelmongodb/routes/api.php
+++ Server/laravelmongodb/routes/api.php
@@ -19,10 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route :: resource('account',AccountController::class)->only([
-    'store'
-]);
-
 Route::group(['prefix'=>'auth'],function($router){
     Route::post('login',[AuthController::class,'Login']);
     Route::post('register',[AuthController::class,'Register']);
